a01a5e2d422a777a4c320e4361f556c7a6d97b22
Brendan Higginbottom <brendan.higginbottom@gmail.com>
2023-04-27 18:49:31 -0500

    added imports and set up store and sagas, left space for reducers and sagas. need to npm install a few things

--- src/index.js
+++ src/index.js
@@ -2,9 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
 
+// Imports for redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Imports for saga
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import axios from 'axios';
+
+// Reducers
+
+// Reducer 1
+
+// Reducer 2
+
+// Reducer 3
+
+// Sagas
+
+// Saga 1
+
+// Saga 2
+
+// Saga 3
+
+// Root Saga
+function* rootSaga() {
+    // yield takeEvery('DISPATCH_NAME', sagaName);
+}
+
+// Saga Middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// Redux store
+const storeInstance = createStore(
+    combineReducers(
+        {
+            //reducers added here
+        }
+    ),
+    applyMiddleware(sagaMiddleware, logger)
+);
+
+// Root saga to middleware
+sagaMiddleware.run(rootSaga);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={storeInstance}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
